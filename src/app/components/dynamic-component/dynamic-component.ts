38c0c0e30dea6e2452e752d601c16e2b1cdcbefc
shib7 <shib714@gmail.com>
2025-09-01 18:59:38 -0400

    initial project before introducing effects

--- src/app/components/dynamic-component/dynamic-component.ts
+++ src/app/components/dynamic-component/dynamic-component.ts
@@ -1,10 +1,11 @@
-import { Component, ComponentRef, effect, signal, viewChild, ViewContainerRef } from '@angular/core';
+import { Component, ComponentRef, effect, signal, TemplateRef, viewChild, ViewContainerRef } from '@angular/core';
 import { Widget } from './widget/widget';
 import { MatButtonModule } from '@angular/material/button';
+import { WeatherContent } from './widget/weather-content';
 
 @Component({
   selector: 'dynamic-component',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, WeatherContent],
   template: `
     <main id="content">
       <h1>Dynamic Component in Angular 20</h1>
@@ -12,7 +13,9 @@ import { MatButtonModule } from '@angular/material/button';
         <input type="checkbox" [checked]="compactMode()" (change)="toggleCompactMode()" />
           Compact Mode
       </label>
-
+    <ng-template #weatherContent>
+        <weather-content />
+    </ng-template>
       <section class="toolbar ">
           <p>Click the button to create the widget dynamically.</p>
           <button mat-flat-button (click)="createComponent()">Create widget</button>
@@ -29,24 +32,30 @@ export class DynamicComponent {
   vcr = viewChild('container', { read: ViewContainerRef });
   compactMode = signal(false);
   #componentRef?: ComponentRef<Widget>;
+  weatherContent = viewChild<TemplateRef<unknown>>('weatherContent');
 
-  constructor() {
-    effect(() => {
-      const isCompact = this.compactMode();
-      this.#componentRef?.setInput('collapsed', isCompact);
-    });
-  }
+
 
   createComponent() {
+    // Clear any existing component before creating a new one
     this.vcr()?.clear();
-    this.#componentRef = this.vcr()?.createComponent(Widget);
+    console.log('Creating component');
+    const content = this.vcr()?.createEmbeddedView(this.weatherContent()!);
 
+    this.#componentRef = this.vcr()?.createComponent(Widget, {
+            projectableNodes: [
+        content?.rootNodes!
+      ]
+    });  
+    //handle the inputs for the dynamically created component
     this.#componentRef?.setInput('title', 'Weather Widget');
     this.#componentRef?.setInput('description', 'Current weather condition in Ottawa, Ontario');
 
+    //handle the output event from the dynamically created component
     this.#componentRef?.setInput('collapsed', this.compactMode());
-    this.#componentRef?.instance.collapsed.subscribe((isCollapsed) => {
-      this.compactMode.set(isCollapsed);
+    this.#componentRef?.instance.closed.subscribe(() => {
+      console.log('Component closed');
+      this.#componentRef?.destroy(); // Destroy the component when the closed event is emitted
     })
 
     console.log('Widget Component Created');
